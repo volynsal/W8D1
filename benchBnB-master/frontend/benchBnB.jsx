8736aa7b768f2afbe53f149c138f92fc75526b4b
volynsal <volynsal@gmail.com>
2019-05-27 14:05:41 -0400

    Sixth commit - Greeting Components

--- benchBnB-master/frontend/benchBnB.jsx
+++ benchBnB-master/frontend/benchBnB.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { sign } from 'crypto';
+import Root from './components/root'
 import { configureStore } from './store/store'
 import { signup, login, logout } from './util/session_api_util';
 
@@ -13,8 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
     window.getState = store.getState;
     window.dispatch = store.dispatch;
 
-    
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+
+    ReactDOM.render(<Root store={store}/>, root);
 });
 
 window.signup = signup;
--- benchBnB-master/frontend/components/root.jsx
+++ benchBnB-master/frontend/components/root.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+import App from './app';
 
 const Root = ({ store }) => (
     <Provider store={store}>
-    // Router goes here...
+        <HashRouter>
+            <App />
+        </HashRouter>
     </Provider>
 );
+
+export default Root;
