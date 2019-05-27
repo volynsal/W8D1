671caa8412caca6c087f75132b117459d02f2674
volynsal <volynsal@gmail.com>
2019-05-27 15:18:10 -0400

    Seventh commit - SessionForm components

--- benchBnB-master/frontend/components/App.jsx
+++ benchBnB-master/frontend/components/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import GreetingContainer from './greeting/greeting_container';
 
 const App = () => (
     <div>
-        <h1>Bench BnB</h1>
+        <header>
+            <h1>Bench BnB</h1>
+            <GreetingContainer />
+        </header>
     </div>
 );
-
-export default App;
---
+++ benchBnB-master/frontend/components/session/session_form.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class SessionForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user);
+    }
+
+    render() {
+        return (
+            <div className="login-form-container">
+                <form onSubmit={this.handleSubmit} className="login-form-box">
+                    Welcome to BenchBnB!
+                    Please {this.props.formType} or {this.props.navLink}
+                    {this.renderErrors()}
+                    <div className="login-form">
+                        <label>Username:
+              <input type="text"
+                                value={this.state.username}
+                                onChange={this.update('username')}
+                                className="login-input"
+                            />
+                        </label>
+                        <label>Password:
+              <input type="password"
+                                value={this.state.password}
+                                onChange={this.update('password')}
+                                className="login-input"
+                            />
+                        </label>
+                        <input className="session-submit" type="submit" value={this.props.formType} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SessionForm;
