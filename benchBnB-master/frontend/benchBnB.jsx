85c5dcbcea716db8dcb633882dbfd1e91031b2a7
volynsal <volynsal@gmail.com>
2019-05-27 13:26:09 -0400

    Fifth commit - Starting Phase 2

--- benchBnB-master/frontend/benchBnB.jsx
+++ benchBnB-master/frontend/benchBnB.jsx
@@ -3,10 +3,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { sign } from 'crypto';
+import { configureStore } from './store/store'
 import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
+    
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
 
